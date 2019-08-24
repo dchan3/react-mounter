543aea35a229f30da0a47d2b83ebf7811bcad4c5
dchan3 <dchan3@hawaii.edu>
2019-08-23 15:56:40 -1000

    Modernizing deps

--- scripts/mocha_runner.js
+++ scripts/mocha_runner.js
@@ -1,6 +1,3 @@
-require('babel-core/register');
-require('babel-polyfill');
-
 process.on('unhandledRejection', function (error) {
   console.error('Unhandled Promise Rejection:');
   console.error(error && error.stack || error);
